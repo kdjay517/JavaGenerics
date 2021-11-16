c7d63d200acbb9edce02e46c1cf7454a37140a52
kdjay517 <kdjay517@gmail.com>
2021-11-16 07:51:03 +0530

    added finding of maximum from the three integer values

--- src/com/bridgelabz/generics/Maximum.java
+++ src/com/bridgelabz/generics/Maximum.java
@@ -1,11 +1,22 @@
 package com.bridgelabz.generics;
+import java.util.*;
 
 public class Maximum {
+	public static int num1;
+	public static int num2;
+	public static int num3;
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		MaximumTest maximumInteger = new MaximumTest();
 		System.out.println("welcome to find maximum Number");
-		
+		System.out.println("Enter first Number:");
+		num1 = sc.nextInt();
+		System.out.println("Enter second Number:");
+		num2 = sc.nextInt();
+		System.out.println("Enter third Number:");
+		num3 = sc.nextInt();
+		System.out.printf("maximum Number in %d,%d,%d is %d:", num1,num2,num3,maximumInteger.maximum(num1, num2, num3));	
 	}
 }
