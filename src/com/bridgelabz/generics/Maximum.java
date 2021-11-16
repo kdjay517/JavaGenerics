945d3ddc7559b427db5aad44467babeca4571a66
kdjay517 <kdjay517@gmail.com>
2021-11-16 07:21:53 +0530

    added finding only second number of three numbers is maximum

---
+++ src/com/bridgelabz/generics/Maximum.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.generics;
+
+public class Maximum {
+
+	public static void main(String[] args) {
+		
+		MaximumTest maximumInteger = new MaximumTest();
+		System.out.println("welcome to find maximum Number");
+		
+	}
+}
