04904b6e64dbe3d6fad0d34f2c5592e6404846c5
kdjay517 <kdjay517@gmail.com>
2021-11-22 11:18:13 +0530

    Added: modified the maximumtest class to generic class to find maximum in integer, float and string inputs

---
+++ src/com/bridgelabz/generics/Maximum.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.generics;
+
+import java.util.Scanner;
+
+public class Maximum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n1.To find maximum Integer\n2.To find maximum flot value" + "\n3.To find maximum String");
+		switch (sc.nextInt()) {
+		case 1:
+			System.out.printf("\n maximum number in (3,5,8) is %d", MaximumTest.findMaximum(3, 5, 8));
+			break;
+		case 2:
+			System.out.printf("\n maximum number in (3.2,5.8,1.2) is %.1f", MaximumTest.findMaximum(3.2, 5.8, 1.2));
+			break;
+		case 3:
+			System.out.printf("\n maximum string in (apple,peach,banana) is %s",
+					MaximumTest.findMaximum("apple", "peach", "banana"));
+
+		}
+
+	}
+
+}
